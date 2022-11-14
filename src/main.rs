55707c4782fa5b8cca2a68a1bd58b24ea1d81440
maxday <maxime.david@datadoghq.com>
2022-11-14 09:25:40 -0500

    unit tests

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() -> Result<()> {
     let folder = "./items/";
     match args.cmd {
         TodoCommand::Add(args) => add(folder, &args),
-        TodoCommand::List(args) => list(folder, &args),
+        TodoCommand::List(args) => list(folder, &args, &mut ConsoleLogger),
         TodoCommand::Remove(args) => remove(folder, &args),
     }
 }
@@ -19,6 +19,7 @@ struct TodoCli {
     #[structopt(subcommand)]
     cmd: TodoCommand,
 }
+
 #[derive(StructOpt)]
 enum TodoCommand {
     #[structopt(about = "add an item", name = "add")]
@@ -28,6 +29,7 @@ enum TodoCommand {
     #[structopt(about = "remove an item", name = "remove")]
     Remove(RemoveOptions),
 }
+
 #[derive(StructOpt)]
 struct AddOptions {
     #[structopt(long)]
@@ -44,6 +46,18 @@ struct RemoveOptions {
 struct ListOptions {
     #[structopt(long)]
     sorted: bool,
+}
+
+trait Logger {
+    fn print(&mut self, value: String);
+}
+
+struct ConsoleLogger;
+
+impl Logger for ConsoleLogger {
+    fn print(&mut self, value: String) {
+        println!("{}", value);
+    }
 }
 
 fn add(folder: &str, args: &AddOptions) -> Result<()> {
@@ -69,14 +83,14 @@ fn remove(folder: &str, args: &RemoveOptions) -> Result<()> {
     }
 }
 
-fn list(folder: &str, args: &ListOptions) -> Result<()> {
+fn list(folder: &str, args: &ListOptions, logger: &mut dyn Logger) -> Result<()> {
     let mut paths: Vec<_> = read_dir(folder)?.filter_map(Result::ok).collect();
     if args.sorted {
         paths.sort_by_key(|dir| dir.file_name());
     }
     for path in paths {
         if let Ok(filename) = path.path().strip_prefix(folder) {
-            println!("{}", filename.display());
+            logger.print(filename.display().to_string());
         }
     }
     Ok(())
@@ -84,4 +98,73 @@ fn list(folder: &str, args: &ListOptions) -> Result<()> {
 
 fn build_path_from_item(folder: &str, item: &str) -> String {
     folder.to_string() + item
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::fs;
+
+    #[derive(Default)]
+    struct TestLogger(Vec<String>);
+    impl Logger for TestLogger {
+        fn print(&mut self, value: String) {
+            self.0.push(value);
+        }
+    }
+
+    #[test]
+    fn build_path_from_item_test() {
+        assert_eq!("folder/item", build_path_from_item("folder/", "item"));
+    }
+
+    #[test]
+    fn add_test() {
+        let folder = "./test_add/";
+        fs::create_dir(folder).unwrap();
+        let mut test_logger = TestLogger::default();
+        let test_add_options = AddOptions {
+            item: String::from("abc"),
+        };
+        let test_list_options = ListOptions { sorted: true };
+        add(folder, &test_add_options).unwrap();
+        list(folder, &test_list_options, &mut test_logger).unwrap();
+        fs::remove_dir_all(folder).unwrap();
+        assert_eq!(1, test_logger.0.len());
+        assert_eq!("abc", test_logger.0[0]);
+    }
+
+    #[test]
+    fn list_test() {
+        let folder = "./test_list/";
+        fs::create_dir(folder).unwrap();
+        File::create(build_path_from_item(folder, "abc")).unwrap();
+        File::create(build_path_from_item(folder, "zzz")).unwrap();
+        File::create(build_path_from_item(folder, "ccc")).unwrap();
+        let test_options = ListOptions { sorted: true };
+        let mut test_logger = TestLogger::default();
+        list(folder, &test_options, &mut test_logger).unwrap();
+        fs::remove_dir_all(folder).unwrap();
+        assert_eq!(3, test_logger.0.len());
+        assert_eq!("abc", test_logger.0[0]);
+        assert_eq!("ccc", test_logger.0[1]);
+        assert_eq!("zzz", test_logger.0[2]);
+    }
+
+    #[test]
+    fn remove_test() {
+        let folder = "./test_remove/";
+        fs::create_dir(folder).unwrap();
+        File::create(build_path_from_item(folder, "abc")).unwrap();
+        let mut test_logger = TestLogger::default();
+        let test_remove_options = RemoveOptions {
+            item: String::from("abc"),
+        };
+        let test_list_options = ListOptions { sorted: true };
+        remove(folder, &test_remove_options).unwrap();
+        list(folder, &test_list_options, &mut test_logger).unwrap();
+        fs::remove_dir_all(folder).unwrap();
+        assert_eq!(0, test_logger.0.len());
+    }
 }
